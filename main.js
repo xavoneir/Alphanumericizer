e90d48cadb104d7c1b5e978dcb65ede2cecf97af
Conrad Leander <32786383+xavoneir@users.noreply.github.com>
2018-04-04 19:32:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+let alphabetMap = new Map([
+  ['a', 1],
+  ['b', 2],
+  ['c', 3],
+  ['d', 4],
+  ['e', 5],
+  ['f', 6],
+  ['g', 7],
+  ['h', 8],
+  ['i', 9],
+  ['j', 1],
+  ['k', 2],
+  ['l', 3],
+  ['m', 4],
+  ['n', 5],
+  ['o', 6],
+  ['p', 7],
+  ['q', 8],
+  ['r', 9],
+  ['s', 1],
+  ['t', 2],
+  ['u', 3],
+  ['v', 4],
+  ['w', 5],
+  ['x', 6],
+  ['y', 7],
+  ['z', 8]
+]);
+
+// iterate over keys (vegetables)
+for (let letter of alphabetMap.keys()) {
+    alert(letter);
+};
+for (let number of alphabetMap.values()) {
+    alert(number);
+};
+for (let entry of alphabetMap) {
+  alert(entry);
+}
